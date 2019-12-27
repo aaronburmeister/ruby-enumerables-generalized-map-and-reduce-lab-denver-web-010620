bc9a91c73ee22324eba2d8e6eaf1f19e51c38572
Aaron Burmeister <f34r91@gmail.com>
2019-12-27 06:46:08 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -25,6 +25,6 @@ def reduce(source_array, starting_point = nil)
   total
 end
 
-puts "nil or nil = #{if 0}"
+puts "nil or nil = #{if 0;}"
 puts "False or true = #{false || true}"
 puts "nil or true = #{nil || true}"
